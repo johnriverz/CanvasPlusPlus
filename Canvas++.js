e0396caf0f6d001679cc47a1c533ff6a9e6d81db
Ethan Aasheim <ethanaasheim@icloud.com>
2023-02-23 11:13:46 -0800

    Extension UI prototype

---
+++ Canvas++.js
@@ -0,0 +1,9 @@
+function showTab(tabNum) {
+    console.log(tabNum);
+    for (var i = 1; i <= 3; i++) {
+        if (i == tabNum)
+            document.getElementById("tab" + i).style.display = "block";
+        else
+            document.getElementById("tab" + i).style.display = "none";
+    }
+}
