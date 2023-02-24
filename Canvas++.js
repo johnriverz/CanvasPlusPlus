77f984b20b6dc7af8c9ede5f1f1138e1adf684e3
johnriverz <juan.jose.rios.jr@gmail.com>
2023-02-24 12:03:34 -0800

    Added access token login tab

--- Canvas++.js
+++ Canvas++.js
@@ -1,10 +1,12 @@
 document.getElementById("assignments-tab-btn").onclick = function() {showTab(1)};
 document.getElementById("notifications-tab-btn").onclick = function() {showTab(2)};
 document.getElementById("grade-calc-tab-btn").onclick = function() {showTab(3)};
+document.getElementById("access-token-tab-btn").onclick = function() {showTab(4)};
+
 
 function showTab(tabNum) {
     console.log(tabNum);
-    for (var i = 1; i <= 3; i++) {
+    for (var i = 1; i <= 4; i++) {
         if (i == tabNum)
             document.getElementById("tab" + i).style.display = "block";
         else
