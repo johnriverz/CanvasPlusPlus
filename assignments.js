64e3f70817618fbf625181637d8d9903e3d6abc8
johnriverz <juan.jose.rios.jr@gmail.com>
2023-03-11 11:49:28 -0800

    assignments tab wip

---
+++ assignments.js
@@ -0,0 +1,91 @@
+/* assignments.js */
+
+// PLACEHOLDER VARIABLES, THESE WILL EVENTUALLY BE PASSED FROM Canvas++.js BUT ARE HERE NOW FOR TESTING
+// UNCOMMENT THEM TO TEST FROM WITHIN THIS JS FILE 
+/*
+// paste your access token here for testing
+let my_token = "2391~Ozq4czr8zWrKrw0ej8vA7ZSfMLA2ZbICANL1ZZUkNYJph8LMBNiLKZL5pzA1COIA"; 
+let options = {
+    method: 'GET'
+};
+let starter_url = "https://cors-anywhere.herokuapp.com/https://canvas.instructure.com/api/v1"
+*/
+
+// IMPORTS
+import {login} from "./login.js"
+
+// FUNCTION IMPLEMENTATION
+
+/* getCourseIDs - fetches the course IDs for user's active courses.
+    Returns them in a map (course_code: id) */
+async function getCourseIDs(postUrl, header, token) {
+    const out = await login(postUrl, header, token, "getCourseIDs").then((courses) => {
+            //console.log("TEST - testing getCourseIDs(), here we log courses w/in it: ");
+            const IDs = new Map();
+            //console.log(courses);
+            for(let i = 0; i < courses.length; i++) { 
+                IDs.set(courses[i].course_code, courses[i].id);
+            }
+            //console.log(IDs);
+            return IDs;
+    });
+    //console.log(out);
+    return out;
+}
+
+
+/* getAssignments - description here */
+async function getAssignments(post_url, header, token) {
+    const out = await getCourseIDs(post_url, header, token).then((IDs) => {
+        return IDs;
+    })
+
+    for (var [key, value] of out) {
+        //console.log(key + ": " + value);
+        console.log("Upcoming assignments in " + key + ":")
+        const res = await login(post_url, header, token, "getAssignments", value).then((assign) => {
+            for (let i = 0; i < assign.length; i++) {
+                console.log(assign[i].name + " is due at: " + assign[i].due_at);
+            }
+        });
+        // return res;
+    }
+
+    //console.log(out);
+    return out;
+    
+    // var token_url = "access_token=" + token;
+    // var assignments_url = post_url + "/courses?enrollment_state=active&" + token_url;
+    // var obj;
+
+    // try {
+    //     const output = await fetch(assignments_url, header)
+    //         .then(response => response.text()) // Read the response as text
+    //         .then(html => {
+    //             //alert("Here's the response from the CANVAS API: " + html);
+    //             obj = JSON.parse(html);
+    //             for(let i = 0; i < obj.length; i++) {
+    //                 console.log(obj[i]);
+    //             }
+    //         })
+    //         .catch(e => {
+    //             console.log(e)
+    //         })
+    //     return output
+    // } catch(e) {
+    //     console.log(e)
+    // }
+}
+
+
+
+
+// TEST FUNCTIONS HERE - UNCOMMENT TO TEST
+/*
+test_output = login(starter_url, options, my_token);
+console.log(test_output);
+*/
+
+
+//EXPORT FUNCTIONS FOR USE IN MAIN .JS FILE
+export {getAssignments, getCourseIDs};
---
+++ login.js
@@ -0,0 +1,90 @@
+/* login.js */
+
+// PLACEHOLDER VARIABLES, THESE WILL EVENTUALLY BE PASSED FROM Canvas++.js BUT ARE HERE NOW FOR TESTING
+// UNCOMMENT THEM TO TEST FROM WITHIN THIS JS FILE 
+
+//paste your access token here for testing
+// let my_token = "2391~KDgphoEk1whGZyI62u1Ljg3j87hvqWBIpQmBztHftaLdGVCclTdVxuS30IcfcJ75"; 
+// let options = {
+//     method: 'GET'
+// };
+// let starter_url = "https://cors-anywhere.herokuapp.com/https://canvas.instructure.com/api/v1"
+
+
+// FUNCTION IMPLEMENTATION
+
+/* LOGIN - Use the access key to authorize your CANVAS API requests. 
+Outputs the text to be put into the "test-api-output" box in our .html
+*/
+
+async function login(post_url, options, token, call, id) {
+    //console.log("TEST: the login button click function ran");
+    // login_url == courses api call URL, since we want to get course info when logging into extension
+    var token_url = "access_token=" + token;
+
+    var call_url;
+    if (call === "getCourseIDs") {
+        call_url = "/courses?enrollment_state=active&";
+    }
+    if (call === "getAssignments") {
+        call_url = "/courses/"+ id + "/assignments?bucket=upcoming&"
+    }
+
+
+    var login_url = post_url + call_url + token_url;
+    //console.log("TESTING login.js below...")
+    //console.log(login_url);
+
+    const output = await fetch(login_url, options)
+        .then(response => {
+            if(!response.ok){
+                console.log("An error arose when fetching from the Canvas API, its response is: ");
+                console.log(response);
+                let api_output_box_text = ("Response code: " + response.status + " Response Text: " + response.statusText);
+                if (response.status == "403"){
+                    console.log("TEST: Displaying 403 cors fix string... ");
+                    const fix_cors = `.\nThe 403 error often arises from CORS issues. Try fixing it by going 
+                    \n to this url and clicking 'Request temporary access to the demo server' button:\n
+                    https://cors-anywhere.herokuapp.com/corsdemo`;
+                    let error_response = api_output_box_text.concat(fix_cors);
+                    return error_response;
+                }
+                if (response.status == "401"){
+                    console.log("TEST: Displaying 401 error text")
+                    let wrong_token = `.\n This error means your CANVAS access token is incorrect or missing.
+                    \n Try submitting your CANVAS API access token again, and if that doesn't work try another token.`
+                    let error_response = api_output_box_text.concat(wrong_token);
+                    console.log("TEST: The api_output_box_text is below...")
+                    console.log(error_response);
+                    return error_response;
+                }
+                throw new Error('ERROR - manually caught: in fetch');
+            }
+            else {
+                //console.log(response.text())
+                return response.text();
+            }
+        })
+    // below should return API response as text, or api_output_box_text error message
+    return JSON.parse(output);
+} 
+
+
+// async function getActiveCourses(postUrl, header, token) {
+//     url = postUrl + "/courses?enrollment_state=active&" + token;
+//     try {
+//         const response = await fetch(url, header);
+//         return response.json();
+//     } catch(e) {
+//         console.log(e);
+//     }
+// }
+
+
+// TEST FUNCTIONS HERE - UNCOMMENT TO TEST
+
+// test_output = login(starter_url, options, my_token);
+// console.log(test_output);
+
+//EXPORT FUNCTIONS FOR USE IN MAIN .JS FILE
+export {login};
